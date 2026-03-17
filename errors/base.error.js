0e641c34178bc7ed34613f192515f2807bb2c72a
OlmosbekOWA <uzbmojor2810@gmail.com>
2026-03-17 14:53:43 +0500

    create erros in midlewers

---
+++ errors/base.error.js
@@ -0,0 +1,21 @@
+module.exports = class BaseError extends Error {
+    status 
+    errors
+
+    constructor(message, status, errors){
+        super(message)
+        this.status = status
+        this.errors = errors
+
+    }
+
+    static UnauthorizedError(){
+        return new BaseError(401, "User is not authorized")
+        
+    }
+
+    static BadRequest(message, errors ){
+        return new BaseError(400, message, errors)
+        
+    }
+}
